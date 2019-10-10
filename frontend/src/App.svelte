9cb22a4007f1b6a1cfe5d0734d145e1ff9eb30fb
Omelchenko Michael <l2maximum@mail.ru>
2019-10-10 21:07:30 +0300

    add svelte

---
+++ frontend/src/App.svelte
@@ -0,0 +1,59 @@
+<div class="wrapper">
+    <Nav></Nav>
+    <main class="main">
+        <svelte:component this={currentSelection} />
+		<button on:click="{enter}">login</button>
+    </main>
+    <Footer></Footer>
+</div>
+
+
+<script>
+    import Nav from './components/Nav.svelte'
+    import Footer from './components/Footer.svelte'
+	import Profile from './components/Profile.svelte'
+	import Login from './components/Login.svelte'
+
+	let currentSelection = Profile
+
+	function enter() {
+    	currentSelection = Login
+	}
+</script>
+
+
+<style>
+    :root {
+        --nav-color: #222437;
+        --count: #3E4551;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #F9F9F9;
+        font-family: 'IBM Plex Sans', sans-serif;
+    }
+
+    .wrapper {
+        display: grid;
+        grid-template-areas: "nav nav nav" ". main ." "footer footer footer";
+        grid-template-rows: 60px 1fr 170px;
+        grid-template-columns: minmax(0, 240px) repeat(auto-fill, minmax(320px, 1fr)) minmax(0, 240px);
+        /*grid-template-columns: auto minmax(320px, 1050px) auto;*/
+        height: 100vh;
+        width: 100%;
+        margin: 0;
+    }
+
+    .main {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        grid-area: main;
+        /*max-width: 1050px;*/
+        /*border: 1px solid #000;*/
+    }
+</style>
+
+
---
+++ frontend/src/components/Nav.svelte
@@ -0,0 +1,103 @@
+<nav>
+    <ul>
+        <a href="#"><img class="logo" src="img/logo.svg"></a>
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">Курсы</a></li>
+        <li><a href="#">Статьи</a></li>
+        <li><a href="#">Форум</a></li>
+        <li><a href="#">Сообщества</a></li>
+        <li><a href="#">Задания</a></li>
+        <li><a href="#">Тесты</a></li>
+        <li class="last">
+            <ul>
+                <li class="li-message">
+                    <a href="#"><img class="message" src="./img/icons/message.svg"></a>
+                    <span class="counter">0</span>
+                </li>
+                <li><a href="#">Вход</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
+
+<script>
+
+</script>
+
+<style>
+    nav {
+    display: flex;
+    grid-area: nav;
+    background-color: var(--nav-color);
+}
+
+nav ul {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+}
+
+nav ul li {
+    list-style: none;
+    padding: 0 0 0 20px;
+}
+
+nav ul .last {
+    margin: 0 35px 0 auto;
+}
+
+nav ul li a {
+    text-decoration: none;
+    color: #fff;
+    padding: 18px 0;
+    position: relative;
+    transition: all 0.5s cubic-bezier(0.4, 0, 1, 1);
+}
+
+nav ul li a:hover {
+    border-bottom: 2px solid;
+}
+
+/*nav ul li a:after {*/
+/*    content: '';*/
+/*    position: absolute;*/
+/*    border-bottom: 2px solid;*/
+/*    left: -300px;*/
+/*    width: 100%;*/
+/*    transition: left 0.5s cubic-bezier(0.4, 0, 1, 1);*/
+/*}*/
+/*nav ul li a:after:hover {*/
+/*    left: 0;*/
+/*}*/
+
+.logo {
+    width: 136px;
+    height: 42px;
+}
+
+.message {
+    width: 26px;
+    height: 18px;
+    position: relative;
+}
+
+.li-message {
+    position: relative;
+}
+
+.counter {
+    position: absolute;
+    color: #fff;
+    font-size: 12px;
+    width: 20px;
+    height: 20px;
+    background-color: var(--count);
+    top: -10px;
+    right: -5px;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 20px;
+}
+</style>
