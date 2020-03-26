4fe64e4944633f35ae05bbe482ce4ccecbe585d9
Hamel007 <hamel_007@mail.ru>
2020-03-26 14:39:14 +0300

    Update menu

---
+++ html/js/main.js
@@ -0,0 +1,6 @@
+$('.btn-open-menu').on('click', function(e) {
+   $('.block-nav').toggleClass('block-nav-open');
+})
+$('.close').on('click', function(e) {
+   $('.block-nav').toggleClass('block-nav-open');
+})
